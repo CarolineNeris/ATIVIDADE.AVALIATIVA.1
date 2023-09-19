a98abc6a631c63d82a09a0cc84f1fd7f76e5a96e
Matheus Costa Beckerath <matheus.beckerath@hotmail.com>
2023-09-19 14:16:13 -0300

    Commit parte 2 avaliacao

--- provaMatheus.cpp
+++ provaMatheus.cpp
@@ -31,11 +31,11 @@ void IncluirCliente(vector<Cliente>& clientes) {
     getline(cin, novoCliente.Nome);
     cout << "Data de Nascimento: ";
     cin >> novoCliente.DtNascimento;
-    cout << "Número de CNH: ";
+    cout << "Numero de CNH: ";
     cin >> novoCliente.CNH;
 
     clientes.push_back(novoCliente);
-    cout << "Cliente incluído com sucesso!" << endl;
+    cout << "Cliente incluido com sucesso!" << endl;
 }
 
 // Função para excluir um cliente por CPF
@@ -47,12 +47,12 @@ void ExcluirCliente(vector<Cliente>& clientes) {
     for (auto it = clientes.begin(); it != clientes.end(); ++it) {
         if (it->CPF == cpfParaExcluir) {
             clientes.erase(it);
-            cout << "Cliente excluído com sucesso!" << endl;
+            cout << "Cliente excluido com sucesso!" << endl;
             return;
         }
     }
 
-    cout << "CPF não encontrado. Nenhum cliente excluído." << endl;
+    cout << "CPF nao encontrado. Nenhum cliente excluido." << endl;
 }
 
 void AlterarCliente(vector<Cliente>& clientes) {
@@ -66,7 +66,7 @@ void AlterarCliente(vector<Cliente>& clientes) {
             cout << "CPF: " << cliente.CPF << endl;
             cout << "Nome: " << cliente.Nome << endl;
             cout << "Data de Nascimento: " << cliente.DtNascimento << endl;
-            cout << "Número de CNH: " << cliente.CNH << endl;
+            cout << "Numero de CNH: " << cliente.CNH << endl;
 
             char opcao;
             cout << "Deseja alterar o nome do cliente? (S/N): ";
@@ -84,7 +84,7 @@ void AlterarCliente(vector<Cliente>& clientes) {
                 cin >> cliente.DtNascimento;
             }
 
-            cout << "Deseja alterar o número de CNH do cliente? (S/N): ";
+            cout << "Deseja alterar o numero de CNH do cliente? (S/N): ";
             cin >> opcao;
             if (opcao == 'S' || opcao == 's') {
                 cout << "Informe o novo número de CNH: ";
@@ -96,7 +96,7 @@ void AlterarCliente(vector<Cliente>& clientes) {
         }
     }
 
-    cout << "CPF não encontrado." << endl;
+    cout << "CPF nao encontrado." << endl;
 }
 
 // Função para listar todos os clientes
@@ -111,7 +111,7 @@ void ListarCliente(const vector<Cliente>& clientes) {
         cout << "CPF: " << cliente.CPF << endl;
         cout << "Nome: " << cliente.Nome << endl;
         cout << "Data de Nascimento: " << cliente.DtNascimento << endl;
-        cout << "Número de Autorização: " << cliente.CNH << endl;
+        cout << "Número de Autorizacao: " << cliente.CNH << endl;
         cout << "---------------------------" << endl;
     }
 }
@@ -128,55 +128,234 @@ void LocalizarCliente(const vector<Cliente>& clientes) {
             cout << "CPF: " << cliente.CPF << endl;
             cout << "Nome: " << cliente.Nome << endl;
             cout << "Data de Nascimento: " << cliente.DtNascimento << endl;
-            cout << "Número de Autorização: " << cliente.CNH << endl;
+            cout << "Numero de Autorizacao: " << cliente.CNH << endl;
             return;
         }
     }
 
-    cout << "CPF não encontrado." << endl;
+    cout << "CPF nao encontrado." << endl;
+}
+
+void IncluirVeiculo(vector<Veiculo>& veiculos) {
+    Veiculo novoVeiculo;
+    cout << "Renavam: ";
+    cin >> novoVeiculo.Renavam;
+    cout << "Placa do Veiculo: ";
+    cin >> novoVeiculo.PlacaDoVeiculo;
+    cout << "Data e Hora de Retirada: ";
+    cin >> novoVeiculo.Data_HoraRetirada;
+    cout << "Data e Hora de Entrega: ";
+    cin >> novoVeiculo.Data_HoraEntrega;
+    cout << "Loja de Retirada: ";
+    cin >> novoVeiculo.Loja_Retirada;
+
+    veiculos.push_back(novoVeiculo);
+    cout << "Veiculo incluido com sucesso!" << endl;
+}
+
+void ExcluirVeiculo(vector<Veiculo>& veiculos) {
+    string placaParaExcluir;
+    cout << "Digite a placa do Veiculo que deseja excluir: ";
+    cin >> placaParaExcluir;
+
+    for (auto it = veiculos.begin(); it != veiculos.end(); ++it) {
+        if (it->PlacaDoVeiculo == placaParaExcluir) {
+            veiculos.erase(it);
+            cout << "Veiculo excluido com sucesso!" << endl;
+            return;
+        }
+    }
+
+    cout << "Placa nao encontrada. Nenhum veiculo excluido." << endl;
+}
+
+void AlterarVeiculo(vector<Veiculo>& veiculos) {
+    string placaParaAlterar;
+    cout << "Digite a placa do veiculo que deseja alterar: ";
+    cin >> placaParaAlterar;
+
+    for (int i = 0; i < veiculos.size(); ++i) {
+        if (veiculos[i].PlacaDoVeiculo == placaParaAlterar) {
+            cout << "Dados atuais do veículo:" << endl;
+            cout << "Renavam: " << veiculos[i].Renavam << endl;
+            cout << "Placa do Veiculo: " << veiculos[i].PlacaDoVeiculo << endl;
+            cout << "Data e Hora de Retirada: " << veiculos[i].Data_HoraRetirada << endl;
+            cout << "Data e Hora de Entrega: " << veiculos[i].Data_HoraEntrega << endl;
+            cout << "Loja de Retirada: " << veiculos[i].Loja_Retirada << endl;
+
+            char opcao;
+            cout << "Deseja alterar o Renavam do veiculo? (S/N): ";
+            cin >> opcao;
+            if (opcao == 'S' || opcao == 's') {
+                cout << "Informe o novo Renavam: ";
+                cin >> veiculos[i].Renavam;
+            }
+
+            cout << "Deseja alterar a Data e Hora de Retirada do veiculo? (S/N): ";
+            cin >> opcao;
+            if (opcao == 'S' || opcao == 's') {
+                cout << "Informe a nova Data e Hora de Retirada: ";
+                cin >> veiculos[i].Data_HoraRetirada;
+            }
+
+            cout << "Deseja alterar a Data e Hora de Entrega do veiculo? (S/N): ";
+            cin >> opcao;
+            if (opcao == 'S' || opcao == 's') {
+                cout << "Informe a nova Data e Hora de Entrega: ";
+                cin >> veiculos[i].Data_HoraEntrega;
+            }
+
+            cout << "Deseja alterar a Loja de Retirada do veiculo? (S/N): ";
+            cin >> opcao;
+            if (opcao == 'S' || opcao == 's') {
+                cout << "Informe a nova Loja de Retirada: ";
+                cin.ignore();
+                getline(cin, veiculos[i].Loja_Retirada);
+            }
+
+            cout << "Veiculo alterado com sucesso!" << endl;
+            return;
+        }
+    }
+
+    cout << "Placa do veiculo nao encontrada." << endl;
 }
 
 
 
-int main(){
+void ListarVeiculo(const vector<Veiculo>& veiculos) {
+    if (veiculos.empty()) {
+        cout << "Nenhum veiculo cadastrado." << endl;
+        return;
+    }
+
+    cout << "Lista de Veiculos:" << endl;
+    for (const Veiculo& veiculo : veiculos) {
+        cout << "Renavam: " << veiculo.Renavam << endl;
+        cout << "Placa do Veiculo: " << veiculo.PlacaDoVeiculo << endl;
+        cout << "Data e Hora de Retirada: " << veiculo.Data_HoraRetirada << endl;
+        cout << "Data e Hora de Entrega: " << veiculo.Data_HoraEntrega << endl;
+        cout << "Loja de Retirada: " << veiculo.Loja_Retirada << endl;
+        cout << "---------------------------" << endl;
+    }
+}
+
+void LocalizarVeiculoPorPlaca(const vector<Veiculo>& veiculos) {
+    string placaParaLocalizar;
+    cout << "Digite a placa do veículo que deseja localizar: ";
+    cin >> placaParaLocalizar;
+
+    for (const Veiculo& veiculo : veiculos) {
+        if (veiculo.PlacaDoVeiculo == placaParaLocalizar) {
+            cout << "Renavam: " << veiculo.Renavam << endl;
+            cout << "Placa do Veículo: " << veiculo.PlacaDoVeiculo << endl;
+            cout << "Data e Hora de Retirada: " << veiculo.Data_HoraRetirada << endl;
+            cout << "Data e Hora de Entrega: " << veiculo.Data_HoraEntrega << endl;
+            cout << "Loja de Retirada: " << veiculo.Loja_Retirada << endl;
+            return;
+        }
+    }
+
+    cout << "Placa do veículo não encontrada." << endl;
+}
+
+
+int main() {
     vector<Cliente> clientes;
+    vector<Veiculo> veiculos;
 
     int escolha;
-    do{
-        cout << "Menu de Opções:" << endl;
-        cout << "1. Incluir Cliente" << endl;
-        cout << "2. Excluir Cliente" << endl;
-        cout << "3. Alterar Cliente (Apenas por CPF)" << endl;
-        cout << "4. Listar Cliente" << endl;
-        cout << "5. Localizar (Apenas por CPF)" << endl;
+    do {
+        cout << "Menu Principal:" << endl;
+        cout << "1. Menu de Clientes" << endl;
+        cout << "2. Menu de Veículos" << endl;
         cout << "0. Sair" << endl;
         cout << "Escolha uma opção: ";
         cin >> escolha;
 
         switch (escolha) {
             case 1:
-                IncluirCliente(clientes);
+                int escolhaCliente;
+                do {
+                    cout << "\nMenu de Clientes:" << endl;
+                    cout << "1. Incluir Cliente" << endl;
+                    cout << "2. Excluir Cliente" << endl;
+                    cout << "3. Alterar Cliente (Apenas por CPF)" << endl;
+                    cout << "4. Listar Clientes" << endl;
+                    cout << "5. Localizar Cliente (Por CPF)" << endl;
+                    cout << "0. Voltar ao Menu Principal" << endl;
+                    cout << "Escolha uma opção: ";
+                    cin >> escolhaCliente;
+
+                    switch (escolhaCliente) {
+                        case 1:
+                            IncluirCliente(clientes);
+                            break;
+                        case 2:
+                            ExcluirCliente(clientes);
+                            break;
+                        case 3:
+                            AlterarCliente(clientes);
+                            break;
+                        case 4:
+                            ListarCliente(clientes);
+                            break;
+                        case 5:
+                            LocalizarCliente(clientes);
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            cout << "Opção inválida." << endl;
+                    }
+                } while (escolhaCliente != 0);
                 break;
+
             case 2:
-                ExcluirCliente(clientes);
+                int escolhaVeiculo;
+                do {
+                    cout << "\nMenu de Veículos:" << endl;
+                    cout << "1. Incluir Veículo" << endl;
+                    cout << "2. Excluir Veículo" << endl;
+                    cout << "3. Alterar Veículo (Por Placa)" << endl;
+                    cout << "4. Listar Veículos" << endl;
+                    cout << "5. Localizar Veículo (Por Placa)" << endl;
+                    cout << "0. Voltar ao Menu Principal" << endl;
+                    cout << "Escolha uma opção: ";
+                    cin >> escolhaVeiculo;
+
+                    switch (escolhaVeiculo) {
+                        case 1:
+                            IncluirVeiculo(veiculos);
+                            break;
+                        case 2:
+                            ExcluirVeiculo(veiculos);
+                            break;
+                        case 3:
+                            AlterarVeiculo(veiculos);
+                            break;
+                        case 4:
+                            ListarVeiculo(veiculos);
+                            break;
+                        case 5:
+                            LocalizarVeiculoPorPlaca(veiculos);
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            cout << "Opção inválida." << endl;
+                    }
+                } while (escolhaVeiculo != 0);
                 break;
-            case 3:
-                AlterarCliente(clientes);
-                break;
-            case 4:
-                ListarCliente(clientes);
-                break;
-              case 5:
-                LocalizarCliente(clientes);
-                break;   
+
             case 0:
                 cout << "Saindo do programa." << endl;
                 break;
+
             default:
                 cout << "Opção inválida." << endl;
         }
     } while (escolha != 0);
-     
-    
+
     return 0;
 }
